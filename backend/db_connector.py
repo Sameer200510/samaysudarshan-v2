61b870fd272b77968ca1338b681ca9ff0d7e231a
SAMEER LOHANI <sameerlohani200510@gmail.com>
2026-02-01 15:12:44 +0530

    Refactor DB connection to use URL parsing

--- backend/db_connector.py
+++ backend/db_connector.py
@@ -1,21 +1,27 @@
 import mysql.connector
 import os
+from urllib.parse import urlparse
 
 def get_db_connection():
     try:
+        db_url = os.getenv("mysql://avnadmin:AVNS_GUiXbqyo_YnqCSAlh-x@mysql-2e18f1a-sameerlohani200510-a83f.k.aivencloud.com:20204/defaultdb?ssl-mode=REQUIRED")
+        if not db_url:
+            raise Exception("DATABASE_URL not set")
+
+        url = urlparse(db_url)
+
         conn = mysql.connector.connect(
-            host=os.getenv("mysql-2e18f1a-sameerlohani200510-a83f.k.aivencloud.com"),
-            user=os.getenv("avnadmin"),
-            password=os.getenv("AVNS_GUiXbqyo_YnqCSAlh-x"),
-            database=os.getenv("defaultdb"),
-            port=int(os.getenv("20204")),
-            ssl_ca="aiven-ca.pem",
+            host=url.hostname,
+            user=url.username,
+            password=url.password,
+            database=url.path.lstrip("/"),
+            port=url.port,
             ssl_disabled=False
         )
+
         print("✅ DB CONNECTED SUCCESSFULLY")
         return conn
+
     except Exception as e:
         print("❌ DB CONNECTION ERROR:", e)
         return None
-           
-           
