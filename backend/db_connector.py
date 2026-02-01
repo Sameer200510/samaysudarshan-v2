727a0fc1374de0e8c40ab79c36a05d5594f5be4d
SAMEER LOHANI <sameerlohani200510@gmail.com>
2026-02-01 15:50:25 +0530

    Refactor database connection to use environment variables

--- backend/db_connector.py
+++ backend/db_connector.py
@@ -4,11 +4,11 @@ import os
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host=os.getenv("mysql-2e18f1a-sameerlohani200510-a83f.k.aivencloud.com"),
-            user=os.getenv("avnadmin"),
-            password=os.getenv("AVNS_GUiXbqyo_YnqCSAlh-x"),
-            database=os.getenv("defaultdb"),
-            port=int(os.getenv("20204")),
+            host=os.environ.get("DB_HOST"),
+            port=int(os.environ.get("DB_PORT")),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            database=os.environ.get("DB_NAME"),
             ssl_disabled=False
         )
         print("✅ DB CONNECTED SUCCESSFULLY")
